77f623c5df37e1bbf1889812c3f604e347c32649
Kris De Volder <kdevolder@pivotal.io>
2018-04-12 16:42:22 -0700

    Small improvement to build-and-install script
    Make sure old version is really gone before reinstalling new version.

--- client/vscode-wordcheck/build-and-install.sh
+++ client/vscode-wordcheck/build-and-install.sh
@@ -4,4 +4,5 @@ npm install
 rm -fr *.vsix
 npm run vsce-package
 code --uninstall-extension Pivotal.vscode-wordcheck || echo "WARN: Can't uninstall old version."
+rm -fr ~/.vscode/extensions/Pivotal.vscode-wordcheck*
 code --install-extension *.vsix
